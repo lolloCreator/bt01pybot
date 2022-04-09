0d865f6460da93777c31f13caf6b40069e3d0715
lolloCreator <luigipacelli27@gmail.com>
2022-04-09 23:54:44 +0200

    second

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ def main() -> None:
     dispatcher.add_handler(conv_handler)
 
     # Start the Bot
-    updater.start_polling()
+    #updater.start_polling()
     updater.start_webhook(listen="0.0.0.0",port=PORT,url_path=TOKEN,webhook_url=APP_NAME + TOKEN)
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
